9af6c5ed307101a9f9fae05fb785eb4dc4fa5c31
vijay2249 <vijaychowdary2249@pm.me>
2024-01-20 01:21:01 +0530

    update profile data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,13 +93,13 @@ export default function Page() {
           </Avatar>
         </div>
         <Section>
-          <h2 className="text-xl font-bold">About</h2>
+          <h2 className="text-xl font-bold">| About |</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
             {RESUME_DATA.summary}
           </p>
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Work Experience</h2>
+          <h2 className="text-xl font-bold">| Work Experience |</h2>
           {RESUME_DATA.work.map((work) => {
             return (
               <Card key={work.company}>
@@ -139,7 +139,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Education</h2>
+          <h2 className="text-xl font-bold">| Education |</h2>
           {RESUME_DATA.education.map((education) => {
             return (
               <Card key={education.school}>
@@ -159,7 +159,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
+          <h2 className="text-xl font-bold">| Technologies | Frameworks |</h2>
           <div className="flex flex-wrap gap-1">
             {RESUME_DATA.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
@@ -168,7 +168,7 @@ export default function Page() {
         </Section>
 
         <Section className="print-force-new-page scroll-mb-16">
-          <h2 className="text-xl font-bold">Projects</h2>
+          <h2 className="text-xl font-bold">| Projects | Notable Works |</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
               return (
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,40 +1,22 @@
 import {
   Amdocs,
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
   JohnDeere,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
 } from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GitHubIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Vijay Chowdary Nelakurthi", //Vijay Chowdary Nelakurthi
-  initials: "VCN", //VCN
-  location: "Ongole, AP, India", //Ongole, AP, India
-  locationLink: "https://www.google.com/maps/place/Ongole", 
+  name: "Vijay Chowdary Nelakurthi",
+  initials: "VCN",
+  location: "Kanigiri, AP, India",
+  locationLink: "https://www.google.com/maps/place/Kanigiri", 
   about:
-  "Software Developer with experience in Automation Testing and Development field",
-  summary:"", //add more summary
-    // "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
+  "Software Developer with experience in Automation Testing and Developing Microservices",
+  summary:"As a Software Developer, I have worked with developing microservice applications that integrate with kakfa and connect with Couchbase DB. As a Automation Test Engineer I have also created framework for UI automation using TestNG, Selenium, Playwright. Current tech stack, Java, SpringBoot, Playwright", //add more summary
   avatarUrl: "https://avatars.githubusercontent.com/u/65058877?v=4",
   personalWebsiteUrl: "https://vijay2249.github.io",
   contact: {
     email: "vijaychowdary2249@protonmail.com",
-    // tel: "+48530213401", //no mobile number display
+    // tel: "",
     social: [
       {
         name: "GitHub",
@@ -50,17 +32,17 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: "National Institute of Technology, Warangal", //National Institute of Technology, Warangal
-      degree: "B.Tech in Computer Science and Engineering", //B.Tech in CSE
-      start: "2019", //2019
-      end: "2023", //2023
+      school: "National Institute of Technology, Warangal",
+      degree: "B.Tech in Computer Science and Engineering",
+      start: "2019",
+      end: "2023",
     },
   ],
   work: [
     {
       company: "Amdocs",
       link: "https://www.amdocs.com/",
-      title: "Software Engineer → Automation Test Engineer → Software Developer",
+      title: "Software Engineer → Automation Test Engineer",
       logo: Amdocs,
       badges: [],
       start: "2023",
@@ -75,10 +57,9 @@ export const RESUME_DATA = {
       badges: ["Intern"],
       title: "Automation Test Intern",
       logo: JohnDeere,
-      start: "May-2022",
-      end: "July-2022",
-      description:
-        "Created an automation framework for testing UI application. Technologies: Python",
+      start: "May 2022",
+      end: "July 2022",
+      description: "Created an automation framework for testing UI application. Technologies: Python",
     },
   ],
   skills: [
@@ -97,22 +78,72 @@ export const RESUME_DATA = {
     "C++"
   ],
   projects: [
-    // {
-    //   title: "Consultly",
-    //   techStack: [
-    //     "Side Project",
-    //     "TypeScript",
-    //     "Next.js",
-    //     "Vite",
-    //     "GraphQL",
-    //     "WebRTC",
-    //   ],
-    //   description: "A platform to build and grow your online business",
-    //   logo: ConsultlyLogo,
-    //   link: {
-    //     label: "consultly.com",
-    //     href: "https://consultly.com/",
-    //   },
-    // },
+    {
+      title: "BoringNotes",
+      description: "My place where I share my notes and concepts that I learned along the way with folks like me.",
+      techStack: [
+        "JavaScript",
+        "Java",
+        "Docker",
+        "Azure Concepts"
+      ],
+      link: {
+        label: "",
+        href: "https://github.com/vijay2249/BoringNotes"
+      }
+    },
+    {
+      title: "Playground",
+      description: "All repos with the name playground as suffix are separate repos to play and learn the concepts of languages and frameworks.",
+      techStack: [
+        "Java",
+        "JavaScript",
+        "Docker",
+        "SpringBoot",
+        "ReactJS",
+        "Nodejs",
+        "MongoDB"
+      ],
+      link: {
+        label: "",
+        href: "https://github.com/vijay2249?tab=repositories&q=playground"
+      }
+    },
+    {
+      title: "Security Basics",
+      description: "Basic scripts to display some security attacks learned while preparing for CompTIA certification",
+      techStack: ["Python", "C"],
+      link: {
+        label: "",
+        href: "https://github.com/vijay2249/random-stuff"
+      }
+    },
+    {
+      title: "Stress relief",
+      description: "Got bored in the first day of new semester and had to clean system for lot of copies of previous semester assignments and projects and notes, written this script to do the task for me",
+      techStack: ['Python'],
+      link: {
+        label: "",
+        href: "https://github.com/vijay2249/stress-relief",
+      }
+    },
+    {
+      title: "Smart Brain",
+      description: "From a given url, it will try to detect the human faces in it",
+      techStack: ["ReactJS", "CSS", "Heroku"],
+      link: {
+        label: "",
+        href: "https://vijay2249.github.io/Smart-Brain/"
+      }
+    },
+    {
+      title: "Robo-Friends",
+      description: "Sample application that I created while learning ReactJS",
+      techStack: ["ReactJS", "CSS", "Netlify"],
+      link: {
+        label: "",
+        href: "https://the-bots-assembly.netlify.app/"
+      }
+    }
   ],
 } as const;
