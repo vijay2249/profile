628df6c398fefed7db3cc371453f3f6a15a928c7
vijay2249 <vijaychowdary2249@pm.me>
2024-05-23 12:46:05 +0530

    update

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,147 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+  Amdocs,
+  JohnDeere,
 } from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GitHubIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Vijay Chowdary Nelakurthi",
+  initials: "VCN",
+  location: "Kanigiri, AP, India",
+  locationLink: "https://www.google.com/maps/place/Kanigiri", 
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  "Software Developer with experience in Automation Testing and Developing Microservices",
+  summary:"As a Software Developer, I have worked with developing microservice applications that integrate with kakfa and connect with Couchbase DB. As a Automation Test Engineer I have also created framework for UI automation using TestNG, Selenium, Playwright. Current tech stack, Java, SpringBoot, Playwright", //add more summary
+  avatarUrl: "https://avatars.githubusercontent.com/u/65058877?v=4",
+  personalWebsiteUrl: "https://vijay2249.github.io",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "vijaychowdary2249@protonmail.com",
+    tel: "",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/vijay2249",
         icon: GitHubIcon,
       },
       {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/VijayCh0710",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "National Institute of Technology, Warangal",
+      degree: "B.Tech in Computer Science and Engineering",
+      start: "2019",
+      end: "2023",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
+      company: "Amdocs",
+      link: "https://www.amdocs.com/",
+      title: "Software Engineer → Automation Test Engineer",
+      logo: Amdocs,
+      badges: [],
+      start: "2023",
       end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+      description: "Working on Playwright and TestNG framework to create UI automation custom. Working on Microservices implementation with technologies like SpringBoot, and integrated with Kafka, CouchbaseDB",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      company: "John Deere",
+      link: "https://www.deere.com/en/index.html", 
+      badges: ["Intern"],
+      title: "Automation Test Intern",
+      logo: JohnDeere,
+      start: "May 2022",
+      end: "July 2022",
+      description: "Created an automation framework for testing UI application. Technologies: Python",
     },
   ],
   skills: [
     "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "ReactJs",
+    "Java",
+    "SpringBoot",
+    "Linux",
+    "Git",
+    "Docker",
+    "Python",
+    "Selenium",
+    "Playwright",
+    "TestNG",
+    "Postman",
+    "C++"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "BoringNotes",
+      description: "My place where I share my notes and concepts that I learned along the way with folks like me.",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "JavaScript",
+        "Java",
+        "Docker",
+        "Azure Concepts"
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
+        label: "",
+        href: "https://github.com/vijay2249/BoringNotes"
+      }
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "Playground",
+      description: "All repos with the name playground as suffix are separate repos to play and learn the concepts of languages and frameworks.",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Java",
+        "JavaScript",
+        "Docker",
+        "SpringBoot",
+        "ReactJS",
+        "Nodejs",
+        "MongoDB"
       ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
+        label: "",
+        href: "https://github.com/vijay2249?tab=repositories&q=playground"
+      }
     },
     {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      title: "Security Basics",
+      description: "Basic scripts to display some security attacks learned while preparing for CompTIA certification",
+      techStack: ["Python", "C"],
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
+        label: "",
+        href: "https://github.com/vijay2249/random-stuff"
+      }
     },
     {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+      title: "Stress relief",
+      description: "Got bored in the first day of new semester and had to clean system for lot of copies of previous semester assignments and projects and notes, written this script to do the task for me",
+      techStack: ['Python'],
       link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
+        label: "",
+        href: "https://github.com/vijay2249/stress-relief",
+      }
     },
     {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
+      title: "Smart Brain",
+      description: "From a given url, it will try to detect the human faces in it",
+      techStack: ["ReactJS", "CSS", "Heroku"],
       link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
+        label: "",
+        href: "https://vijay2249.github.io/Smart-Brain/"
+      }
     },
     {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
+      title: "Robo-Friends",
+      description: "Sample application that I created while learning ReactJS",
+      techStack: ["ReactJS", "CSS", "Netlify"],
       link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+        label: "",
+        href: "https://the-bots-assembly.netlify.app/"
+      }
+    }
   ],
 } as const;
